9a55131e5879fa6f53e9b2baf694ed145f7d3d4d
relpace <87755349+relpace@users.noreply.github.com>
2021-12-12 13:38:53 +0800

    Add files via upload
    手动一键点餐搞完了 芜湖 然后做定时任务

--- main.py
+++ main.py
@@ -6,42 +6,34 @@ import configparser
 
 s = requests.session()
 c = configparser.ConfigParser()
+text=''
 
 
 def login(username, passwd):
+    global headers
+    global cookie
     password_md5 = hashlib.md5(passwd.encode("utf-8")).hexdigest()
     login_url = 'https://xst.nfcpwl.com/extInterface/wx/loginByStuCard'
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
-        "Referer": "https://xst.nfcpwl.com/wx/index.html",
-        "Origin": "https://xst.nfcpwl.com",
-        "Host": "xst.nfcpwl.com",
-        "Accept": "application/json, text/plain, */*",
-    }
+
     data = {'stuCardNo': username, 'stuCardPwd': password_md5}
     open_id = {'password': '',
                'schoolNo': '3301020003', }
-    login_response = s.post(login_url, headers=headers, data=data)
+    login_response = s.post(login_url, headers=headers, data=data, )
     response = {}
     z = json.loads(login_response.text)
     response['openId'] = z['mdata']['data'][0]['channelId']
     response['buyerId'] = z['mdata']['data'][0]['stuNo']
     response['customId'] = z['mdata']['data'][0]['stuNo']
+    response['stuIdType'] = z['mdata']['data'][0]['stuIdType']
     openidurl = 'https://xst.nfcpwl.com/extInterface/wx/students/openId'
     open_id['openId'] = response['openId']
-    openid_response = s.post(openidurl, headers=headers, data=open_id)
-    openid_response.raise_for_status()
+    openid_response = s.post(openidurl, headers=headers, data=open_id, )
+    cookie = openid_response.cookies
     return response
 
 
-def get_meal(user_dict, ):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
-        "Referer": "https://xst.nfcpwl.com/wx/index.html",
-        "Origin": "https://xst.nfcpwl.com",
-        "Host": "xst.nfcpwl.com",
-        "Accept": "application/json, text/plain, */*",
-    }
+def get_meal(user_dict):
+    global headers
     data_meals = {}
     mealurl = 'https://xst.nfcpwl.com/extInterface/wx/meals'
     data_meals['customId'] = user_dict['customId']
@@ -54,12 +46,17 @@ def get_meal(user_dict, ):
 
 
 def order_code(code):
+    global text
     global is_ordered
     global days_of_week
     global mealdates
     global packagedata
     global y
+    global headers
+    global user_dict
+    cartids=[0]*days_of_week
     orderlist = []
+    url = 'https://xst.nfcpwl.com/extInterface/wx/cart'
     i = 0
     while len(orderlist) != days_of_week:
         if i >= len(code):
@@ -67,17 +64,42 @@ def order_code(code):
         orderlist.append(ord(code[i]) - ord('A'))
         i += 1
     for j in range(days_of_week):
-        data={
-            'dinnerDate': mealdates[i]['dinner_date'],
-            'buyerId': mealdates[i]['BUYER_ID'],
+        if is_ordered[j]:
+            continue
+        data = {
+            'dinnerDate': packagedata[6 * j + orderlist[j]]['PLAN_DATE'],
+            'buyerId': mealdates[j]['BUYER_ID'],
             'dinnerNum': 1,
-            'dinnerTime': mealdates[i]['DINNER_TIME'],
+            'dinnerTime': mealdates[j]['DINNER_TIME'],
             'customId': y['customId'],
             'openId': y['openId'],
-            'packageId': packagedata[6*days_of_week+orderlist[j]]['PACKAGE_ID']
-            'price': packagedata[6*days_of_week+orderlist[j]]['PACKAGE_PRICE']
+            'packageId': packagedata[6 * j + orderlist[j]]['PACKAGE_ID'],
+            'price': packagedata[6 * j + orderlist[j]]['PACKAGE_PRICE'],
         }
 
+        resp = s.post(url, headers=headers, data=data,)
+
+        cartids[j]=json.loads(resp.text)['mdata']['data']['cartId']
+    # purchase
+    purchase_url='https://xst.nfcpwl.com/extInterface/wx/order'
+    purchase_data={
+        'cartIds': cartids[:],
+        'customId': y['customId'],
+        'openId': y['openId'],
+        'customType': user_dict['stuIdType'],
+        'operationId': "100",
+        'paymentStatus':'19',
+
+    }
+    purchase_resp = s.post(purchase_url,headers=headers,data=purchase_data)
+    print(purchase_resp.text)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
+    "Referer": "https://xst.nfcpwl.com/wx/index.html",
+    "Origin": "https://xst.nfcpwl.com",
+    "Host": "xst.nfcpwl.com",
+    "Accept": "application/json, text/plain, */*",
+}
 c.read('config.ini', encoding='UTF-8')
 session = requests.session()
 user_name = input("请输入学号/身份证号")
@@ -92,3 +114,5 @@ is_ordered = [False] * days_of_week
 for j in range(days_of_week):
     if mealdates[j]['ORDERED_NUM'] == 1:
         is_ordered[j] = True
+if c.get('mode', 'order_mode') == 'code':
+    order_code(c.get('preferences', 'code').split())
